569ad54a527da01ddf83e9ca885267e1d120a023
AlvaroGrimaldos <alvarogrimaldos21@gmail.com>
2025-06-12 17:54:43 -0500

    build: :building_construction: Se hace la base de la pagina de cambiar la contraseña

--- JS/cambiarContraseña.js
+++ JS/cambiarContraseña.js
@@ -1,21 +1,19 @@
 import { getDatabase, ref, set, get, child } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-database.js";
 
-window.iniciarSesion = function() {
+window.verificar = function() {
     const tipoDocumento = document.getElementById("documento");
     const valorTipoDocumento = tipoDocumento.value;
     const numeroDocumento = document.getElementById("inputNumeroDocumento");
     const valorNumeroDocumento = numeroDocumento.value;
-    const contraseña = document.getElementById("inputContraseña");
-    const valorContraseña = contraseña.value;
-    const obligatorio = document.getElementById("obligatorio")
+    const correo = document.getElementById("inputCorreo")
+    const valorCorreo = correo.value;
 
-    if(!valorTipoDocumento.trim() || !valorNumeroDocumento.trim() || !valorContraseña.trim()) {
+    if(!valorTipoDocumento.trim() || !valorNumeroDocumento.trim() || !valorCorreo.trim()) {
         obligatorio.classList.replace('invisible', 'visible')
         return false;
     }
 
     obligatorio.classList.replace('visible', 'invisible');
-    // alert("Todo exitoso");
     
-
+    
 }
