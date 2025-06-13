b6380bd7547ba54d304531c00335184a18d36b3d
AlvaroGrimaldos <alvarogrimaldos21@gmail.com>
2025-06-12 22:23:30 -0500

    fix: :bug: Se soluciona el bug de redireccion y validacion de datos en la seccion de iniciar sesion

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,15 @@
 import { getDatabase, ref, set, get, child } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-database.js";
 
-window.iniciarSesion = function() {
+window.iniciarSesion = async function() {
     const tipoDocumento = document.getElementById("documento");
     const valorTipoDocumento = tipoDocumento.value;
     const numeroDocumento = document.getElementById("inputNumeroDocumento");
     const valorNumeroDocumento = numeroDocumento.value;
     const contraseña = document.getElementById("inputContraseña");
     const valorContraseña = contraseña.value;
-    const obligatorio = document.getElementById("obligatorio")
+    const obligatorio = document.getElementById("obligatorio");
+    const invalido = document.getElementById("invalido");
+    const noUser = document.getElementById("noUser");
 
     if(!valorTipoDocumento.trim() || !valorNumeroDocumento.trim() || !valorContraseña.trim()) {
         obligatorio.classList.replace('invisible', 'visible')
@@ -16,6 +18,31 @@ window.iniciarSesion = function() {
 
     obligatorio.classList.replace('visible', 'invisible');
     // alert("Todo exitoso");
-    
 
+    const app = window.firebaseApp;
+    const database = getDatabase(app);
+    const dbRef = ref(database);
+    
+    try {
+        const snapshot = await get(child(dbRef, "users"));
+        if(snapshot.exists()) {
+            let acceso = false;
+
+            snapshot.forEach(childSnapshot => {
+                const datos = childSnapshot.val();
+                if(datos.tipo_documento === valorTipoDocumento && datos.numero_documento === valorNumeroDocumento && datos.contraseña === valorContraseña) {
+                    acceso = true;
+                }
+            });
+            if (acceso) {
+                window.location.href = "dashboard.html";
+            }else {
+                invalido.classList.replace('invisible', 'visible')
+            }
+        }else {
+            noUser.classList.replace('invisible', 'visible')
+        }
+    } catch(error){
+        console.error(error);
+    }
 }
